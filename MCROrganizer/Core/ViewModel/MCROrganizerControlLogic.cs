06231088de41d4ed686e7fcd982874ad9fa7df44
Daniel <feed.faceit@gmail.com>
2020-09-05 23:19:58 +0300

    - Added the View (bound to the ViewModel through its DataContext) and the ViewModel. - Succesfully bound the Text property of a TextBlock to an accessor defined in the ViewModel (as a test).

---
+++ MCROrganizer/Core/ViewModel/MCROrganizerControlLogic.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCROrganizer.Core.ViewModel
+{
+    public class ControlLogic
+    {
+        public String ExampleText => "Test";
+    }
+}
