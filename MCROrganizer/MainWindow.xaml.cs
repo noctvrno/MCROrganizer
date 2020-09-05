356eb3b9d333a64b4e61f18e8fc1d6f088913b69
Daniel <feed.faceit@gmail.com>
2020-09-05 22:58:49 +0300

    Add project files.

---
+++ MCROrganizer/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MCROrganizer.Core.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
