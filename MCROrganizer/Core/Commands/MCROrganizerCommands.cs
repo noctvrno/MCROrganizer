bec49a3965799dea8f11526e0dc3ae00600dec8b
Daniel <feed.faceit@gmail.com>
2020-09-07 01:34:56 +0300

    - Implemented the ICommand interface. - Created the main StackPanel for the challenge run UI elements. - Succesfully added buttons dynamically to the stack.

---
+++ MCROrganizer/Core/Commands/MCROrganizerCommands.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MCROrganizer.Core.Commands
+{
+    public class MCROCommand : ICommand
+    {
+        private readonly Predicate<object> _canExecute;
+        private readonly Action<object> _execute;
+
+        public MCROCommand(Predicate<object> canExecute, Action<object> execute)
+        {
+            _canExecute = canExecute;
+            _execute = execute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
--- MCROrganizer/Core/ViewModel/MCROrganizerControlLogic.cs
+++ MCROrganizer/Core/ViewModel/MCROrganizerControlLogic.cs
@@ -1,13 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using MCROrganizer.Core.Commands;
 
 namespace MCROrganizer.Core.ViewModel
 {
     public class ControlLogic
     {
-        public String ExampleText => "Test";
+        #region Data Members
+        private ObservableCollection<Button> challengeRunGames = new ObservableCollection<Button>()
+        {
+            new Button() { Content = "First Button"},
+            new Button() { Content = "Second Button"}
+        };
+        #endregion
+
+        #region Commands
+        public ICommand AddGameToChallengeRunCommand => new MCROCommand(new Predicate<object>(obj => true), new Action<object>(obj =>
+        {
+            ChallengeRunGames.Add(new Button() { Content = "Button added dynamically" });
+        }));
+        #endregion
+
+        #region Accessors
+        public ObservableCollection<Button> ChallengeRunGames
+        {
+            get => challengeRunGames;
+            set => challengeRunGames = value;
+        }
+        #endregion
     }
 }
